be9ce25144ff61e62c75565e133b8f6a74c3b068
Breno Batista <brenojab@montreal.com.br>
2020-01-28 08:22:16 -0300

    Making progressbar looking as a control center iOS slider.

---
+++ ProgressBarSlider/ProgressBarSlider.Android/CustomProgressBarRenderer.cs
@@ -0,0 +1,33 @@
+﻿using Android.Content;
+using ProgressBarSlider;
+using ProgressBarSlider.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomProgressBar), typeof(CustomProgressBarRenderer))]
+namespace ProgressBarSlider.Droid
+{
+    public class CustomProgressBarRenderer : ProgressBarRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.ProgressBar> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var progressBar = Control as Android.Widget.ProgressBar;
+                var draw = Resources.GetDrawable(Resource.Drawable.bar_color);
+                progressBar.ProgressDrawable = draw;
+            }
+
+
+
+        }
+
+        public CustomProgressBarRenderer(Context context) : base(context)
+        {
+
+        }
+            
+    }
+}
---
+++ ProgressBarSlider/ProgressBarSlider/ProgressView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ProgressBarSlider
+{
+    public partial class ProgressView : ContentView
+    {
+        public double Progress
+        {
+            set { SetValue(ProgressProperty, value); }
+            get { return (double)GetValue(ProgressProperty); }
+        }
+
+        public readonly static BindableProperty ProgressProperty = BindableProperty.Create("Progress", typeof(double), typeof(ProgressView), 0.0);
+
+    }
+}
